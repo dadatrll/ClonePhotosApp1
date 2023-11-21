fa074bcaccf4512d8ab10aa5f239fb78ef4ed58e
dadatrll <148473320+dadatrll@users.noreply.github.com>
2023-11-21 16:24:33 +0100

    Add files via upload

---
+++ ClonePhotosApp/ClonePhotosApp/ClonePhotosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClonePhotosAppApp.swift
+//  ClonePhotosApp
+//
+//  Created by Daniela Turiello on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ClonePhotosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CoreMLDocumentationView()
+        }
+    }
+}
