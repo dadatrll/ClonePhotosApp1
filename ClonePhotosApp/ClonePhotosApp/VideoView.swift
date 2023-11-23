aa848687b80b6e45089a3f683b339f2e2fdb69db
dadatrll <148473320+dadatrll@users.noreply.github.com>
2023-11-23 17:36:38 +0100

    Add files via upload

---
+++ ClonePhotosApp/ClonePhotosApp/VideoView.swift
@@ -0,0 +1,53 @@
+//
+//  ClonePhotosAppApp.swift
+//  ClonePhotosApp
+//
+//  Created by Daniela Turiello on 20/11/23.
+//
+
+import SwiftUI
+
+struct VideoView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        
+        NavigationStack {
+            VStack() {
+                if Platform.isSimulator {
+                  
+                    Text("Oops! You are using the Simulator 👀. \nRun the App on iPhone to use the \nlive Camera.").multilineTextAlignment(.center).padding()
+                }else{
+                    SwiftUIAVCaptureVideoPreviewView()
+                }
+            }
+                .navigationTitle("Live Camera")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: {
+                            dismiss()
+                        }) {
+                            Image(systemName: "x.circle").padding(.top)
+                        }
+                        .accessibilityAddTraits([.isButton])
+                        .accessibilityLabel("close view")
+                    }
+                }
+        }
+        .accessibilityLabel("Live Camera")
+    }
+}
+
+#Preview {
+    VideoView().preferredColorScheme(.dark)
+}
+
+// This block is used in order to detect if the app is running on a simulator or not. In this case, the detection will prevent a crash of the app during the preview of the live camera.
+
+struct Platform {
+
+    static var isSimulator: Bool {
+        return TARGET_OS_SIMULATOR != 0
+    }
+
+}
